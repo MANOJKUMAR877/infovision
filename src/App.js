b812bab1f54a0c2067b446b1ff063215b193e68b
manojkumar877 <kbmanojkumar.20cs@srit.org>
2021-01-26 21:06:27 -0800

    search bar added in header of the row

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="container"> 
+      <h1 style={{marginLeft:'40%'}}>Verizon AR Campaign Management System</h1>
         <EditableTable/>
       </div>
     )
--- src/EditableTable.js
+++ src/EditableTable.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
-import { Table, Input, InputNumber, Popconfirm, Form, Typography } from "antd";
-import originData from './data'
+import { Table, Input, InputNumber, Popconfirm, Form, Typography,Button,Space } from "antd";
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
+import originData from "./data";
+
 
 const EditableCell = ({
   editing,
@@ -15,25 +18,24 @@ const EditableCell = ({
   const inputNode = inputType === "number" ? <InputNumber /> : <Input />;
   return (
     <td {...restProps}>
-      {editing
-        ? (
-            <Form.Item 
-              name={dataIndex}
-             
-              style={{
-                margin: 0,
-              }}
-              rules={[
-                {
-                  required: true,
-                  message: `Please Input ${title}!`,
-                },
-              ]}
-            >
-              {inputNode}
-            </Form.Item>
-          )
-        : children}
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
     </td>
   );
 };
@@ -59,7 +61,85 @@ const EditableTable = () => {
     });
     setEditingKey(record.key);
   };
+  const [searchText, updateSearchText] = useState('');
+const [searchedColumn, updateSearchcolumn] = useState('');
 
+ const getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+               
+                
+                updateSearchText( selectedKeys[0])
+                
+                updateSearchcolumn(dataIndex)
+              
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
+        : '',
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        
+      }
+    },
+    render: text =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    updateSearchText( selectedKeys[0])
+                
+    updateSearchcolumn(dataIndex)
+  };
+
+ const handleReset = clearFilters => {
+    clearFilters();
+   updateSearchText('')
+  };
   const cancel = () => {
     setEditingKey("");
   };
@@ -91,58 +171,66 @@ const EditableTable = () => {
       dataIndex: "type",
       width: "15%",
       editable: false,
-      
+      ...getColumnSearchProps('type')
     },
     {
-      title: "Ar Media 1- USDZ(url)",
+      title: "AR Media 1- USDZ(url)",
       dataIndex: "turnleft",
       width: "15%",
       editable: true,
+      ...getColumnSearchProps('turnleft')
     },
     {
       title: "AR Media 1- GLB(url) ",
       dataIndex: "turnright",
       width: "15%",
       editable: true,
+      ...getColumnSearchProps('turnright')
     },
     {
       title: "AR Media 2- USDZ(url) ",
       dataIndex: "armedia1",
       width: "15%",
       editable: true,
+      ...getColumnSearchProps('armedia1')
     },
     {
       title: "AR Media 2- GLB(url) ",
       dataIndex: "armedia2",
       width: "15%",
       editable: true,
+      ...getColumnSearchProps('armedia2')
     },
     {
       title: "2D Target (url)",
       dataIndex: "twodtarget",
       width: "15%",
       editable: true,
+      ...getColumnSearchProps('twotarget')
     },
     {
       title: "Mascot Mode(Y/N)",
       dataIndex: "mascot",
       width: "15%",
       editable: true,
+      ...getColumnSearchProps('mascot')
     },
     {
       title: "Latitude",
       dataIndex: "latitude",
       width: "15%",
       editable: true,
+      ...getColumnSearchProps('latitude')
     },
     {
       title: "longitude",
       dataIndex: "longitude",
       width: "15%",
       editable: true,
+      ...getColumnSearchProps('longitude')
     },
     {
-      title: "operation",
+      title: "Operation",
       dataIndex: "operation",
       render: (_, record) => {
         const editable = isEditing(record);
@@ -158,7 +246,7 @@ const EditableTable = () => {
               Save
             </a>
             <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-              <a>Cancel</a>
+              <a style={{color:'red'}}>Cancel</a>
             </Popconfirm>
           </span>
         ) : (
@@ -188,8 +276,9 @@ const EditableTable = () => {
       }),
     };
   });
+
   return (
-    <Form form={form} component={false}>
+    <Form form={form} component={false} >
       <Table
         components={{
           body: {
@@ -203,6 +292,7 @@ const EditableTable = () => {
         pagination={{
           onChange: cancel,
         }}
+       
       />
     </Form>
   );
